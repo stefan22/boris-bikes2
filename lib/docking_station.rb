c7ecf27aab19b4204690caecd4cef8c4a8b355b4
Stefan Lissia <stefan.emailacct@gmail.com>
2015-06-01 16:10:33 +0100

    completed stage5 10 tests passed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,19 +1,37 @@
 require_relative 'bike'
 
 class DockingStation
+  DEFAULT_CAPACITY = 20
+
+  attr_reader :capacity
 
   def initialize
     @bikes = []
+    @capacity = DEFAULT_CAPACITY
   end
 
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
   def dock bike
-    fail 'Docking station full' if @bikes.count >= 20
+    fail 'Docking station full' if full?
     @bikes << bike
   end
+
+
+  private
+
+  attr_reader :bikes
+
+  def full?
+    @bikes.count >= @capacity
+  end
+
+  def empty?
+    @bikes.empty?
+  end
+
 
 end
