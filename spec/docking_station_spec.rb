626efdef2a2e484a9646f210a442b8d5c056885b
Stefan Lissia <stefan.emailacct@gmail.com>
2015-06-01 15:51:48 +0100

    completed 9 tests stage5

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -18,4 +18,12 @@ describe DockingStation do
     end
   end
 
+  describe 'dock' do
+    it 'raises an error when full' do
+        20.times { subject.dock Bike.new }
+        expect { subject.dock Bike.new}.to raise_error 'Docking station full'
+
+    end
+  end
+
 end
